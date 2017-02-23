f7bbcca88833029159b201dd56ce6aace072abcd
noteloquent <woodmanjd32@gmail.com>
2017-02-22 20:06:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ FEHMotor right_motor(FEHMotor::Motor3, 12.0);
 FEHServo arm(FEHServo::Servo0);
 
 //Encoders
-/*
-DigitalEncoder right_encoder(FEHIO::);
-DigitalEncoder left_encoder(FEHIO::);
-*/
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_7);
+
 
 // Bump Switches
 /*
@@ -22,10 +21,9 @@ DigitalInputPin front_left(FEHIO::);
 DigitalInputPin front_right(FEHIO::);
 DigitalInputPin back_left(FEHIO::);
 DigitalInputPin back_right(FEHIO::);
-/*
+*/
 
-// CdS Cell
-//AnalogInputPin cds_cell(FEHIO::);
+AnalogInputPin cds_cell(FEHIO::P2_7); // CdS Cell
 
 // Optosensors
 /*
@@ -37,6 +35,8 @@ const double left_threshold = ;
 const double middle_threshold = ;
 const double right_threshold = ;
 */
+const int sleep_time = 500;
+
 
 bool touch_start() // Wait for screen touch
 {
@@ -47,19 +47,20 @@ bool touch_start() // Wait for screen touch
             temp = true;
         }
     }
+    return temp;
 }
 
 
 
-/*void drive(int percent, int counts)
+void drive(int left_percent, int right_percent, int counts)
 {
     // Reset encoder counts
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
 
     // Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(percent);
+    right_motor.SetPercent(right_percent);
+    left_motor.SetPercent(left_percent);
 
     // While the average of the left and right encoder is less than counts,
     // keep running motors
@@ -68,11 +69,13 @@ bool touch_start() // Wait for screen touch
     // Turn off motors
     right_motor.Stop();
     left_motor.Stop();
+
+    Sleep(sleep_time);
 }
 
 
 
-void turn_left(int percent, int counts) // using encoders
+void turn_left(int left_percent, int right_percent, int counts) // using encoders
 {
     // Reset encoder counts
     right_encoder.ResetCounts();
@@ -80,8 +83,8 @@ void turn_left(int percent, int counts) // using encoders
 
     // Set both motors to desired percent
 
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(-percent);
+    right_motor.SetPercent(right_percent);
+    left_motor.SetPercent(-left_percent);
 
     // While the average of the left and right encoder is less than counts,
     // keep running motors
@@ -91,11 +94,13 @@ void turn_left(int percent, int counts) // using encoders
     // Turn off motors
     right_motor.Stop();
     left_motor.Stop();
+
+    Sleep(sleep_time);
 }
 
 
 
-void turn_right(int percent, int counts) // using encoders
+void turn_right(int left_percent, int right_percent, int counts) // using encoders
 {
     // Reset encoder counts
     right_encoder.ResetCounts();
@@ -103,8 +108,8 @@ void turn_right(int percent, int counts) // using encoders
 
     // Set both motors to desired percent
 
-    right_motor.SetPercent(-percent);
-    left_motor.SetPercent(percent);
+    right_motor.SetPercent(-right_percent);
+    left_motor.SetPercent(left_percent);
 
     // While the average of the left and right encoder is less than counts,
     // keep running motors
@@ -114,8 +119,10 @@ void turn_right(int percent, int counts) // using encoders
     // Turn off motors
     right_motor.Stop();
     left_motor.Stop();
+
+    Sleep(sleep_time);
 }
-*/
+
 
 
 int main() {
@@ -124,6 +131,77 @@ int main() {
 
     arm.SetMin(500);
     arm.SetMax(2480);
+
+    int right_turn_counts = 260;
+    int left_turn_counts = 275;
+    int left_percent = 25;
+    int right_percent = 25;
+    int percent = 25;
+
+
+
+    drive(left_percent, right_percent, 405); // 10 inches
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    turn_left(left_percent, right_percent, left_turn_counts);
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    drive(left_percent, right_percent, 486); // 12 inches
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    turn_left(left_percent, right_percent, left_turn_counts);
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    drive(left_percent, right_percent, 1215); // 30 inches
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    turn_right(left_percent, right_percent, right_turn_counts);
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    drive(left_percent, right_percent, 324); // 8 inches
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    turn_right(left_percent, right_percent, right_turn_counts);
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
+
+    drive(-left_percent, -right_percent, 810); // 10 inches
+    LCD.Write("LE: ");
+    LCD.WriteLine(left_encoder.Counts());
+    LCD.Write("RE: ");
+    LCD.WriteLine(right_encoder.Counts());
+    LCD.WriteLine(" ");
 
     return 0;
 }
